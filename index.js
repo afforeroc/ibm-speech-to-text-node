8a3417b4f01ba80f32a56b5e562f68129f3ed312
Andres Forero <afforeroc@unal.edu.co>
2020-09-24 00:01:44 -0500

    processing using recognize and for

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const fs = require('fs');
 var XLSX = require('xlsx')
 
+// Loading basekeywords
 var workbook = XLSX.readFile('basekeywords.xlsx');
 var sheet_name_list = workbook.SheetNames;
 var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
@@ -15,8 +16,7 @@ for (const idx in xlData) {
     basekeywords.push(xlData[idx].keyword)
     //console.log(xlData[idx].keyword);
 }
-
-console.log(basekeywords)
+//console.log(basekeywords)
 
 // IBM STT init and config
 const { IamAuthenticator } = require('ibm-watson/auth');
@@ -29,6 +29,56 @@ const speechToText = new SpeechToTextV1({
     serviceUrl: 'https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/0ab13aa8-b4ae-4f59-a836-8c2b7f9eaba8',
 });
 
+
+// using RECOGNIZE
+var audiosFiles = fs.readdirSync('audios/');
+for (const i in audiosFiles) {
+    let audioFile = audiosFiles[i];
+    console.log(audioFile);
+    const params = {
+        audio: fs.createReadStream('audios/' + audioFile),
+        contentType: 'audio/mp3',
+        model: 'es-CO_NarrowbandModel',
+        keywords: ['bancolombia', 'alianza', 'buenos'],
+        keywordsThreshold: 0.5,
+        maxAlternatives: 3,
+        speakerLabels: true
+    };
+    speechToText.recognize(params)
+    .then(response => {
+        let dataJSON = JSON.stringify(response.result, null, 2);
+        saveJSONFilename(audioFile, dataJSON);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+
+
+// Only audio file
+/*
+const params = {
+    audio: fs.createReadStream('audios/' + audioFile),
+    contentType: 'audio/mp3; rate=44100',
+    model: 'es-CO_NarrowbandModel',
+    keywords: ['bancolombia', 'alianza', 'buenos'],
+    keywordsThreshold: 0.5,
+    maxAlternatives: 3,
+    speakerLabels: true
+};
+
+speechToText.recognize(params)
+    .then(response => {
+        let dataJSON = JSON.stringify(response.result, null, 2);
+        saveJSONFilename(audioFile, dataJSON);
+    })
+    .catch(err => {
+        console.log(err);
+});
+*/
+
+// using WEBSOCKETS
+/*
 const params = {
     objectMode: true,
     contentType: 'audio/mp3',
@@ -39,42 +89,36 @@ const params = {
     speakerLabels: true
 };
 
-// Create the stream
 const recognizeStream = speechToText.recognizeUsingWebSocket(params);
-
-// IBM STT processing with an audio file
 var audioFile = 'sample.mp3'
 fs.createReadStream('audios/' + audioFile).pipe(recognizeStream)
-recognizeStream.on('data', function(event) { onEvent('Data', event, audioFile); });
-recognizeStream.on('error', function(event) { onEvent('Error', event, audioFile); });
-recognizeStream.on('close', function(event) { onEvent('Close', event, audioFile); });
 
-// IBM STT processing with various audio files from a folder
-/*
-var audiosFiles = fs.readdirSync('audios/');
-for (const idx in audiosFiles) {
-    let audioFile = audiosFiles[idx];
-    console.log(audioFile);
-    fs.createReadStream('audios/' + audioFile).pipe(recognizeStream)
-    recognizeStream.on('data', function(event) { onEvent('Data', event, audioFile); });
-    recognizeStream.on('error', function(event) { onEvent('Error', event, audioFile); });
-    recognizeStream.on('close', function(event) { onEvent('Close', event, audioFile); });
-}*/
+recognizeStream.on('data', function(event) { 
+    let dataJSON = onEvent('Data', event, audioFile);
+    saveJSONFilename(audioFile, dataJSON); 
+});
 
-// audioFile to jsonFile (replace extension)
-function getJsonFile(audioFile) {
-    var nameFile = audioFile.split('.').slice(0, -1).join('.')
-    return `${nameFile}.json`
-}
+recognizeStream.on('error', function(event) { 
+    onEvent('Error', event, audioFile); 
+});
+
+recognizeStream.on('close', function(event) { 
+    onEvent('Close', event, audioFile); 
+});
 
 // Events handling
-function onEvent(name, event, audioFile) {
+function onEvent(name, event, audioFilename) {
     if (name == 'Data') {
-        let data = JSON.stringify(event, null, 2);
-        let jsonFile = getJsonFile(audioFile);
-        fs.writeFileSync('json/' + jsonFile, data);
+        return JSON.stringify(event, null, 2);
     }
-    else {
-        console.log(name, event, audioFile);
-    }
+    return event;
+}
+*/
+
+// Save JSON file with filename of audio file
+function saveJSONFilename(audioFile, dataJSON) {
+    let nameFile = audioFile.split('.').slice(0, -1).join('.')
+    let JSONFile = `${nameFile}.json`
+    fs.writeFileSync('json/' + JSONFile, dataJSON);
+    console.log(`${JSONFile} was saved successfully`)
 }
